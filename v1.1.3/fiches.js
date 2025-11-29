497ff242cf786faf2c8b0cfe6b7b8eee4f9cc35e
Roméo Leonard <romeo.leonard56@gmail.com>
2025-11-29 12:55:45 +0100

    Add files via upload

---
+++ v1.1.3/fiches.js
@@ -0,0 +1,15 @@
+// ../fiches.js
+const fiches = [
+  {
+    "id": "00001",
+    "titre": "Introduction aux fonctions",
+    "matiere": "Bienvenue !",
+    "pdf": "pdf/presentation.pdf",
+  },
+  {
+    "id": "00002",
+    "titre": "Fiche de révisions 'La formation des mots'",
+    "matiere": "Français",
+    "pdf": "pdf/fr/formation-mots.pdf",
+  },
+];
