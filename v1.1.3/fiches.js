82da38242413b04fdc65c23f8a313fe2c144a5fb
Roméo Leonard <romeo.leonard56@gmail.com>
2025-12-08 16:54:30 +0100

    Add files via upload

---
+++ v1.1.3/fiches.js
@@ -0,0 +1,27 @@
+// ../fiches.js
+const fiches = [
+  {
+    "id": "00001",
+    "titre": "Introduction aux fonctions",
+    "matiere": "Bienvenue !",
+    "pdf": "pdf/presentation.pdf",
+  },
+  {
+    "id": "00002",
+    "titre": "Fiche de révisions 'La formation des mots'",
+    "matiere": "Français",
+    "pdf": "pdf/fr/formation-mots.pdf",
+  },
+  {
+    "id": "00003",
+    "titre": "Le régime de l’URSS",
+    "matiere": "Histoire Géo",
+    "pdf": "pdf/hg/regime-urss1.pdf",
+  },
+  {
+    "id": "00004",
+    "titre": "Le régime de l’URSS",
+    "matiere": "Histoire Géo",
+    "pdf": "pdf/hg/regime-urss1",
+  }
+];
---
+++ v1.1.6/fiches.js
@@ -0,0 +1,57 @@
+// ../fiches.js
+const fiches = [
+  {
+    "id": "00001",
+    "titre": "Introduction aux fonctions",
+    "matiere": "Bienvenue !",
+    "pdf": "../pdf/1.pdf",
+  },
+    {
+    "id": "00002",
+    "titre": "TEST FICHES 0001",
+    "matiere": "TEST",
+    "pdf": "pdf/8.pdf"
+  },
+  {
+    "id": "00003",
+    "titre": "Le régime de l’URSS",
+    "matiere": "Histoire Géo",
+    "pdf": "../pdf/3.pdf",
+  },
+  {
+    "id": "00004",
+    "titre": "Le théorème de Thales : Sa réciproque",
+    "matiere": "Maths",
+    "pdf": "../pdf/4.pdf"
+  },
+  {
+    "id": "00005",
+    "titre": "Les propositions, les subordonnées et la phrase complexe 1",
+    "matiere": "Français",
+    "pdf": "../pdf/5.pdf"
+  },
+  {
+    "id": "00006",
+    "titre": "Les propositions, les subordonnées et la phrase complexe 2",
+    "matiere": "Français",
+    "pdf": "../pdf/6.pdf"
+  },
+  {
+    "id": "00007",
+    "titre": "ÉTUDE DE DONNÉES STATISTIQUES 1",
+    "matiere": "Maths",
+    "pdf": "../pdf/7.pdf"
+  },
+  {
+    "id": "00008",
+    "titre": "ÉTUDE DE DONNÉES STATISTIQUES 2",
+    "matiere": "Maths",
+    "pdf": "../pdf/8.pdf"
+  },
+  {
+    "id": "00009",
+    "titre": "ÉTUDE DE DONNÉES STATISTIQUES 3",
+    "matiere": "Maths",
+    "pdf": "../pdf/9.pdf"
+  },
+];
